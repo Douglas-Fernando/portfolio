ab6402c87c587ef6b8fac6b080715e157d9928dd
Douglas Fernando <douglas021fernando_contato@outlook.com.br>
2025-08-20 22:49:24 -0400

    Version 1.0

---
+++ site/src/jsMain/kotlin/com/douglas/portifolio/pages/Index.kt
@@ -0,0 +1,18 @@
+package com.douglas.portifolio.pages
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.navigation.Link
+import org.jetbrains.compose.web.css.vh
+import org.jetbrains.compose.web.dom.Text
+
+@Page
+@Composable
+fun HomePage() {
+
+}
+
---
+++ site/src/jsMain/kotlin/com/douglas/portifolio/pages/Projetos.kt
@@ -0,0 +1,2 @@
+package com.douglas.portifolio.pages
+
